262684f9709c2d8cb4720d8d2cca3f26f8f64d27
alex <alex@youremail>
2021-12-31 22:01:29 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+
+#while ! git pull --rebase ; do echo 'lll';done;
+
 (cd ../my-extension3/ && npm run build2)&&echo 'build done'
 cp -a ../my-extension3/dist/* ./
-while ! git pull ; do echo 'lll';done;
 git add .
 git commit -am 'update'
 while ! git push ; do echo 'lll';done;
