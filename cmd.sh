023a5e8a0250eb361fbdc90aaed32c4b70b38e46
alex <alex@youremail>
2022-03-19 11:22:35 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -3,7 +3,7 @@
 #while ! git pull --rebase ; do echo 'lll';done;
 
 (cd ../my-extension3/ && npm run build2 -- --dest='../testdemo' --no-clean )&&echo 'build done'
-cp -a ../my-extension3/dist/* ./
+#cp -a ../my-extension3/dist/* ./
 git add .
 git commit -am 'update'
 while ! git push ; do echo 'lll';done;
