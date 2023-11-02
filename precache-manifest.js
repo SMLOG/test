b5db7f770c3c816e89b27321c8e4bceaf859ada6
test <smlog@github.com>
2023-11-02 10:37:48 +0800

    update

--- precache-manifest.js
+++ precache-manifest.js
@@ -12,19 +12,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "css/chunk-vendors.css?_hash=709554a7"
   },
   {
-    "revision": "9bacebd2f7482cdd7d45",
-    "url": "css/content-script.css?_hash=63567618"
+    "revision": "29fb7ecbf871ac347e09",
+    "url": "css/content-script.css?_hash=ed17fb32"
   },
   {
-    "revision": "570920242a320d0f7e3b",
-    "url": "css/index.css?_hash=e15ef7c3"
+    "revision": "67791bf5e5fdb8e53818",
+    "url": "css/index.css?_hash=2d6e7f66"
   },
   {
     "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
     "url": "favicon.ico"
   },
   {
-    "revision": "16cce39d70132b8f3f176fa373721571",
+    "revision": "85c66be5943f1edbf2b476fb293bd0a6",
     "url": "index.html"
   },
   {
@@ -36,8 +36,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "js/chunk-vendors.js?_hash=68d003f0"
   },
   {
-    "revision": "570920242a320d0f7e3b",
-    "url": "js/index.js?_hash=e4e5f61d"
+    "revision": "67791bf5e5fdb8e53818",
+    "url": "js/index.js?_hash=a66baa0a"
   },
   {
     "revision": "c120f4923276bc6232768797954728c8",
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 importScripts(
   "wb-debug.js",
-  "precache-manifest.js?_v=e77fd70fcbd22123ccf5f942e022fdcf"
+  "precache-manifest.js?_v=9eb4744ba18f9a01e0df9cec2d09e2fb"
 );
 
 workbox.core.setCacheNameDetails({prefix: "my"});
